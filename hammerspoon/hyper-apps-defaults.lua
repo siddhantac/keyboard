3a729fe5a477f4ffd897cc7c36b9e813778301bc
Siddhanta <siddhanta.c@foodpanda.com>
2023-03-17 14:12:38 +0800

    Update with my changes

--- hammerspoon/hyper-apps-defaults.lua
+++ hammerspoon/hyper-apps-defaults.lua
@@ -4,13 +4,13 @@
 -- this file, save it as `hyper-apps.lua`, and edit the table below to configure
 -- your preferred shortcuts.
 return {
-  { 'a', 'Music' },             -- "A" for "Apple Music"
-  { 'b', 'Google Chrome' },     -- "B" for "Browser"
-  { 'c', 'Slack' },             -- "C for "Chat"
-  { 'd', 'Remember The Milk' }, -- "D" for "Do!" ... or "Done!"
-  { 'e', 'Atom' },              -- "E" for "Editor"
-  { 'f', 'Finder' },            -- "F" for "Finder"
-  { 'g', 'Mailplane 3' },       -- "G" for "Gmail"
-  { 's', 'Slack' },             -- "S" for "Slack"
-  { 't', 'iTerm' },             -- "T" for "Terminal"
+  { 'c', 'Google Chrome' },
+  { 'f', 'Finder' },
+  { 'k', 'Kitty' },
+  { 'o', 'Obsidian' },
+  { 'p', 'Spotify' },
+  { 's', 'Slack' },
+  { 'w', 'WhatsApp' },
+  { 'z', 'zoom.us' },
+  { 'x', 'Firefox' },
 }
